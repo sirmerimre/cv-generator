50fe46760b7e9247fd82f0d05084203d0e48fa87
Sirmer Imre <sirmer.imre@gmail.com>
2019-02-25 08:27:34 +0100

    Pdf preview added

--- src/app/feature/pdf/pdf.component.ts
+++ src/app/feature/pdf/pdf.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import * as jsPDF from 'jspdf';
 
 @Component({
@@ -8,6 +8,8 @@ import * as jsPDF from 'jspdf';
 })
 export class PdfComponent implements OnInit {
 
+  @ViewChild('iframe', {read: ElementRef}) iframe: ElementRef;
+
   constructor() {
   }
 
@@ -15,16 +17,69 @@ export class PdfComponent implements OnInit {
   }
 
   generatePdf() {
-    const doc = new jsPDF();
-    doc.text('Hello world!', 10, 10);
-    doc.save('a4.pdf');
+    const pdf = new jsPDF();
+    const source = document.querySelectorAll('#document')[0];
+    const specialElementHandlers = {
+      // element with id of "bypass" - jQuery style selector
+      '#bypassme': function (element, renderer) {
+        // true = "handled elsewhere, bypass text extraction"
+        return true;
+      }
+    };
+    const margins = {
+      top: 80,
+      bottom: 60,
+      left: 40,
+      width: 522
+    };
+    // all coords and widths are in jsPDF instance's declared units
+    // 'inches' in this case
+    pdf.fromHTML(
+      source, // HTML string or DOM elem ref.
+      margins.left, // x coord
+      margins.top, { // y coord
+        'width': margins.width, // max width of content on PDF
+        'elementHandlers': specialElementHandlers
+      },
+
+      function (dispose) {
+        // dispose: object with X, Y of the last line add to the PDF
+        //          this allow the insertion of new lines after html
+        pdf.save('Test.pdf');
+      }, margins);
   }
 
-  printPdf() {
-    const doc = new jsPDF();
-    doc.text('Hello world!', 10, 10);
-    doc.autoPrint({variant: 'non-conform'});
-    doc.save('a4.pdf');
+  previewPdf() {
+    const pdf = new jsPDF();
+    const source = document.querySelectorAll('#document')[0];
+    const specialElementHandlers = {
+      // element with id of "bypass" - jQuery style selector
+      '#bypassme': function (element, renderer) {
+        // true = "handled elsewhere, bypass text extraction"
+        return true;
+      }
+    };
+    const margins = {
+      top: 80,
+      bottom: 60,
+      left: 40,
+      width: 522
+    };
+
+    pdf.fromHTML(
+      source, // HTML string or DOM elem ref.
+      margins.left, // x coord
+      margins.top, { // y coord
+        'width': margins.width, // max width of content on PDF
+        'elementHandlers': specialElementHandlers
+      });
+
+
+    this.iframe.nativeElement.setAttribute('src', pdf.output('datauristring'));
+  }
+
+  initPdf() {
 
   }
+
 }
